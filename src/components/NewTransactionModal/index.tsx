a8421ca17a260adeab4074aa504658266d4754ab
Gabriel Vellasco de Mattos <53792961+vellascogabriel@users.noreply.github.com>
2021-04-23 16:38:42 -0300

    NewTransactionModal.tsx

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,54 @@
+import Modal from 'react-modal'
+import { Container, TransactionTypeContainer } from './styles';
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import closeImg from '../../assets/close.svg'
+
+interface NewTransactionModalProps{
+    isOpen: boolean;
+    onRequestClose: ()=>void;
+}
+
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps){
+
+    return(
+        <Modal 
+            isOpen={isOpen} 
+            onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
+            >
+                <button 
+                type="button" 
+                onClick={onRequestClose} 
+                className="react-modal-close"
+                >
+                    <img src={closeImg} alt="Fechar"/>
+                </button>
+
+                <Container>
+                <h2>Cadastrar Transação</h2>
+
+                <input placeholder="Título" type="text"/>
+
+                <input type="number" placeholder="Valor"/>
+
+                <TransactionTypeContainer>
+                    <button type="button">
+                        <img src={incomeImg} alt="Entrada"/>
+                        <span>Entrada</span>
+                    </button>
+
+                    <button type="button">
+                        <img src={outcomeImg} alt="Saída"/>
+                        <span>Saída</span>
+                    </button>
+                </TransactionTypeContainer>
+
+                <input type="text" placeholder="Categoria"/>
+
+                <button type="submit">Cadastrar</button>
+                </Container>
+        </Modal>
+    );
+}
